5c97e39736cbf8bd549ae83d6f36a8c8de99c07b
Mohai <mohai.cse@gmail.com>
2026-01-16 18:13:11 +0600

    Change Password testing is in progress.

--- OrangeHRM_Playwright_DotNet/Pages/ChangePasswordPage.cs
+++ OrangeHRM_Playwright_DotNet/Pages/ChangePasswordPage.cs
@@ -1,10 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Playwright;
 
 namespace OrangeHRM_Playwright_DotNet.Pages
 {
+
+
     internal class ChangePasswordPage
     {
+        private IPage page;
+
+        public ChangePasswordPage(IPage page)
+        {
+            this.page = page;
+        }
+
+        public async Task Enter_CurrentPassword(string oldPassword)
+        {
+            await page.Locator("//label[normalize-space()='Current Password']//parent::div//following-sibling::div//input").FillAsync(oldPassword);
+
+        }
+
+        public async Task Enter_NewPassword(string newPassword)
+        {
+            await page.Locator("//label[normalize-space()='Password']//parent::div//following-sibling::div//input").FillAsync(newPassword);
+
+        }
+
+        public async Task Enter_ConfirmPassword(string confirmPassword)
+        {
+            await page.Locator("//label[normalize-space()='Confirm Password']//parent::div//following-sibling::div//input").FillAsync(confirmPassword);
+        }
+
+        public async Task ClickOnSaveButton()
+        {
+            await page.Locator("//button[@type='submit']").ClickAsync();
+        }
+
+        public async Task ClickOnCancel()
+        {
+            await page.Locator("//button[normalize-space()='Cancel']").ClickAsync();
+        }
+
+        public async Task<bool> IsToastMessageVisible()
+        {
+            return await page.Locator("//div[@id='oxd-toaster_1']//p[normalize-space()='Successfully Saved']").IsVisibleAsync();
+        }
+
     }
 }
---
+++ OrangeHRM_Playwright_DotNet/TestCases/ChangePasswordTest.cs
@@ -0,0 +1,42 @@
+﻿using OrangeHRM_Playwright_DotNet.Drivers;
+using OrangeHRM_Playwright_DotNet.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrangeHRM_Playwright_DotNet.TestCases
+{
+    internal class ChangePasswordTest:Setup
+    {
+
+        [Test]
+
+        public async Task TC_008_Change_Password_Functionality_is_working_correctly()
+        {
+
+            LoginPage login = new LoginPage(page);
+
+            await login.Enter_UserName("Admin");
+            await login.Enter_Password("admin123");
+            await login.ClickOnLoginButton();
+
+            ProfileDropDownObjects profile = new ProfileDropDownObjects(page);
+
+            await profile.ClickOnProfileDropdown();
+            await profile.ClickOnChangePasswordLink();
+
+            ChangePasswordPage change = new ChangePasswordPage(page);
+
+            await change.Enter_CurrentPassword("admin123");
+            await change.Enter_NewPassword("admin@123456");
+            await change.Enter_ConfirmPassword("admin@123456");
+            await change.ClickOnSaveButton();
+
+            bool actualResult = await change.IsToastMessageVisible();
+
+            Assert.That(actualResult,Is.True);
+
+        }
+
+    }
+}
