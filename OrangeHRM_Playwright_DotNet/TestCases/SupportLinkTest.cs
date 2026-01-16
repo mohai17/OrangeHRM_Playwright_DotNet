d86536a38bae7301e448baf4015bd09ae2267daa
Mohai <mohai.cse@gmail.com>
2026-01-16 17:18:44 +0600

    Automation test for Support Link is done.

--- OrangeHRM_Playwright_DotNet/TestCases/SupportLinkTest.cs
+++ OrangeHRM_Playwright_DotNet/TestCases/SupportLinkTest.cs
@@ -1,13 +1,32 @@
 ﻿using OrangeHRM_Playwright_DotNet.Drivers;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using OrangeHRM_Playwright_DotNet.Pages;
+
 
 namespace OrangeHRM_Playwright_DotNet.TestCases
 {
     internal class SupportLinkTest:Setup
     {
+        [Test]
         
+        public async Task TC_008_Support_Link_is_working_correctly()
+        {
+            LoginPage login = new LoginPage(page);
+
+            await login.Enter_UserName("Admin");
+            await login.Enter_Password("admin123");
+            await login.ClickOnLoginButton();
+
+            ProfileDropDownObjects profile = new ProfileDropDownObjects(page);
+
+            await profile.ClickOnProfileDropdown();
+            await profile.ClickOnSupportLink();
+
+            SupportPage support = new SupportPage(page);
+
+            bool actualResult = await support.IsSupportPageDisplayed();
+
+            Assert.That(actualResult, Is.True);
+        } 
 
     }
 }
