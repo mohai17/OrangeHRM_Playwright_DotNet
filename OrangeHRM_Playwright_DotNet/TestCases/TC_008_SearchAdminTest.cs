8f55b7a863dbab3c8b5fd86325495642ed6928bb
Mohai <mohai.cse@gmail.com>
2026-01-17 17:58:28 +0600

    TC_008 Search Admin automation script is in progress.

---
+++ OrangeHRM_Playwright_DotNet/TestCases/TC_008_SearchAdminTest.cs
@@ -0,0 +1,13 @@
+﻿using OrangeHRM_Playwright_DotNet.Drivers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrangeHRM_Playwright_DotNet.TestCases
+{
+    internal class TC_008_Check_that_search_admin_functionality_is_working_correctly:Setup
+    {
+
+
+    }
+}
